a7c956e226f2e70223f269631c3999580087e7c2
mordinii <allan.mordini@hotmail.com>
2022-12-18 23:12:03 -0300

    Adicionando o Event para emitir os dados.

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,26 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component ({
     selector:'app-body',
     templateUrl:'./body.component.html',
     styleUrls:['./body.component.css']
-
-
-
 })
-
     
 
 export class BodyComponent {
+
+@Output() aoTransferir = new EventEmitter<any>();
+
 
     valor!: number;
     destino!: number;
 
 
-    transferir(){
+    transferir(): void{
         
-        console.log("Valor:", this.valor);
-        console.log("Destino:", this.destino);
+        const valorEmitir = {valor: this.valor, destino: this.destino} 
+        this.aoTransferir.emit(valorEmitir);
 
     }
 
